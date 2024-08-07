df18edc6994324802edde12918cecf3ab4f7df1b
DESKTOP-K0A9REL\qorck <qorcksgud@naver.com>
2024-08-07 17:50:20 +0900

    공부

--- c++/Project1/Function/function.cpp
+++ c++/Project1/Function/function.cpp
@@ -12,14 +12,57 @@ double AddFunction(const double A, const double B)
 		return Result;
 }
 
-unsigned long long Factorial(unsigned char C)
+unsigned long long Factorial(int A)
 {
-	if (unsigned char(C == 0))
+	int Value = 1; // 0부터 시작하면 값이 0이 되기 때문
+	for (int b = 0; b < A - 1; ++b)  // b가 A-1 인 이유는 곱하는 수임.
+	{
+	  Value *= (b + 2);
+	}
+	return Value;
+}
+
+unsigned long long Factorial_Re (unsigned char C)
+{
+	if (unsigned char(C == 1))
 	{
 		return 1;
 
 	}
-	return C * (C - 1);
+	return C * Factorial_Re(C - 1);
+
+
+}
+
+int Fibonacci(int _iNum)
+{
+	if (1 == _iNum || 2 == _iNum)
+	{
+		return 1;
+	}
+
+	int iPrev1 = 1;
+	int iPrev2 = 1;
+	int iValue = 0;
+
+	for (int i = 0; i < _iNum - 2; ++i)
+	{
+		iValue = iPrev1 + iPrev2;
+		iPrev1 = iPrev2;      // iPrev2 = iValue;    주의
+		iPrev2 = iValue;	  // iPrev1 = iPrev2;
+	}
+
+	return iValue;
+
+	int Fibonacci_Re(int _iNum);
+	{
+		if (1 == _iNum || 2 == _iNum)
+		{
+			return 1;
+		}
+
+		return Fibonacci_Re(_iNum - 1) + Fibonacci_Re(_iNum - 2);
+	}
 }
 
 
--- c++/Project1/Function/function.h
+++ c++/Project1/Function/function.h
@@ -3,4 +3,17 @@ int AddFunction(const int A, const int B);
 
 double AddFunction(const double A, const double B);
 
-unsigned long long Factorial(unsigned char C);
+
+
+// 팩토리얼 함수를 만들어보자
+unsigned long long Factorial(int A);
+
+// 재귀함수 팩토리얼
+unsigned long long Factorial_Re(unsigned char C);
+
+// 피보나치 수열
+int Fibonacci(int _iNum);
+
+// 피보나치 재귀함수
+
+int Fibonacci_Re(int _iNum);
